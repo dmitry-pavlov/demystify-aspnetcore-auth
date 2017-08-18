0280faa7782a64d7fa9ad34163e23e4f71cc1085
David McCullough <damccull@gmail.com>
2017-08-18 13:19:10 -0600

    formatting changes

--- DemystifyTutorial/Controllers/HomeController.cs
+++ DemystifyTutorial/Controllers/HomeController.cs
@@ -5,32 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DemystifyTutorial.Models;
+using Microsoft.AspNetCore.Authorization;
 
-namespace DemystifyTutorial.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace DemystifyTutorial.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult About()
-        {
+        [Authorize]
+        public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
-        {
+        public IActionResult Contact() {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
