0b366bfe54c9132d0ec5aafa88eb3a2b85c3a7cf
David McCullough <damccull@gmail.com>
2017-08-18 13:19:22 -0600

    add account controller with login methods.

---
+++ DemystifyTutorial/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemystifyTutorial.Controllers {
+    public class AccountController : Controller {
+        public IActionResult Login(string returnUrl) {
+            return View("Login",returnUrl);
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password, string returnUrl) {
+            //TODO: build an authentication that uses the cookie auth handler
+            return Json(new {returnUrl, username, password });
+        }
+    }
+}
